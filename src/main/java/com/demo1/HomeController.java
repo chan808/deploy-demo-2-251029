b477f321b6438786aaeb6898e3fcb73b045b915c
Chan <rlackswhd200@gmail.com>
2025-10-28 11:05:30 +0900

    homecontroller 추가

---
+++ src/main/java/com/demo1/HomeController.java
@@ -0,0 +1,15 @@
+package com.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return """
+                <h1>Welcome to the Home Page</h1>
+                <p>This is a simple Spring Boot application.</p>
+                """;
+    }
+}
